e77b9c42341d3decfa5aed5093fe40c95eeb86df
amiprajapati1102 <amiprajapati1102@gmail.com>
2021-03-19 16:36:53 +0530

    my commit

---
+++ FinalHtml/js/script.js
@@ -0,0 +1,53 @@
+
+function sticky_header() {
+    var header_height = jQuery('.home_navbar').innerHeight() / 2;
+    var scrollTop = jQuery(window).scrollTop();;
+    if (scrollTop > header_height) {
+        jQuery('body').addClass('sticky-nav')
+        $(".home_navbar .navbar img").attr("src", "../images/logo/logo-blue.png");
+    } else {
+        jQuery('body').removeClass('sticky-nav')
+        $(".home_navbar .navbar img").attr("src", "../images/logo/logo-white.png");
+    }
+}
+
+jQuery(document).ready(function () {
+  sticky_header();
+});
+
+jQuery(window).scroll(function () {
+  sticky_header();  
+});
+jQuery(window).resize(function () {
+  sticky_header();
+});
+
+
+$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+  
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
